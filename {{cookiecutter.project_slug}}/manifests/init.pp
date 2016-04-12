324d137b6a66891173ad5b7fd6b80d7268184812
Courtney Wang <courtney.wang@reddit.com>
2016-04-12 16:14:33 -0700

    Add Vagrantfile and puppet cookiecutter templates.

---
+++ {{cookiecutter.project_slug}}/manifests/init.pp
@@ -0,0 +1,51 @@
+# Installation of initial dependencies and baseplate application
+# for {{ cookiecutter.project_name }} in a development environment.
+# Please add additional dependencies and env setup code as needed.
+class {{ cookiecutter.project_slug }} {
+  $project_path = '/home/vagrant/{{ cookiecutter.project_slug }}'
+
+  group { 'puppet':
+    ensure => 'present',
+  }
+
+  Exec { path => [ '/usr/bin', '/usr/sbin', '/bin', '/usr/local/bin' ] }
+
+  # Add reddit package apt-repo
+  exec { 'reddit-repo-add':
+    command => 'sudo add-apt-repository ppa:reddit/ppa -y',
+    unless  => 'apt-cache policy | grep reddit',
+    notify => Exec['update-apt'],
+  }
+
+  # Update apt
+  exec { 'update-apt':
+    command => 'sudo apt-get update',
+    refreshonly => true,
+  }
+
+  # Install the dependencies
+  package {
+    ['python',
+     'python-dev',
+     'python-pip',
+     'python-gevent',
+     'python-baseplate',
+     'python-pyramid',
+     'einhorn',
+    ]:
+      ensure  => installed,
+      require => [
+        Exec['reddit-repo-add'], # add reddit repo for baseplate and einhorn
+        Exec['update-apt'],      # The system update needs to run
+      ],
+      notify  => Exec['install-app'],
+  }
+
+  # Set up the app
+  exec { 'install-app':
+    cwd     => $project_path,
+    command => 'python setup.py develop',
+  }
+}
+
+include {{ cookiecutter.project_slug }}
